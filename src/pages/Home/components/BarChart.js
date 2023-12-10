7ac80c3c374761daa6575cecc2533f4070ab92a1
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-10 18:31:59 -0500

    P106 Day7-11 Home-Echarts组件封装实现

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,44 @@
+import * as echarts from 'echarts';
+
+import { useEffect, useRef } from 'react';
+
+
+const BarChart = ({ title, }) => {
+
+  const chartRef = useRef(null)
+  useEffect(() => {
+    const chartDom = chartRef.current
+    const myChart = echarts.init(chartDom);
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['xhamster', 'pornhub', 'xvideo']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+
+
+
+  }, [])
+
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+
+
+
+}
+
+export default BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,13 @@
-import * as echarts from 'echarts';
 
-import { useEffect, useRef } from 'react';
-
-
+import BarChart from './components/BarChart'
 const Home = () => {
 
-  const chartRef = useRef(null)
-  useEffect(() => {
-    const chartDom = chartRef.current
-    const myChart = echarts.init(chartDom);
-    const option = {
-      xAxis: {
-        type: 'category',
-        data: ['xhamster', 'pornhub', 'xvideo']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [10, 40, 70],
-          type: 'bar'
-        }
-      ]
-    };
-
-    option && myChart.setOption(option);
-
-
-
-
-  }, [])
-
-
-  return <div><div ref={chartRef} style={{ width: '500px', height: '400px' }}></div></div>
+  return (
+    <div>
+      <BarChart title={'porn web satisfaction'} />
+      <BarChart title={'porn web popularity'} />
+    </div>
+  )
 }
 
 export default Home
