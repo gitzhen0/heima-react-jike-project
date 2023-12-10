ec0c5577604573d9e3f950360c323b37e5a4e823
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-10 18:11:59 -0500

    P105 Day7-10.Home-Echarts基础图表渲染

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,39 @@
+import * as echarts from 'echarts';
+
+import { useEffect, useRef } from 'react';
+
+
 const Home = () => {
-  return <div>this is Home</div>
+
+  const chartRef = useRef(null)
+  useEffect(() => {
+    const chartDom = chartRef.current
+    const myChart = echarts.init(chartDom);
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['xhamster', 'pornhub', 'xvideo']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+
+
+
+  }, [])
+
+
+  return <div><div ref={chartRef} style={{ width: '500px', height: '400px' }}></div></div>
 }
 
 export default Home
