113cf24e2a9a8df7b072bdc1cb3c065993f9f2c6
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-09 22:35:26 -0500

    P82 : Day6-02 : ant design and little test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Button } from 'antd'
+
 function App() {
-  return <div>this is App</div>
+  return <div>this is App<Button type='primary'>testbutton</Button></div>
 }
 
 export default App
