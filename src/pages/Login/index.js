dd5e560f8f6937803d9c14754a793d34e3dadcf2
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-10 12:30:05 -0500

    P93 Day6-13 登录-redux管理token-实现登录后续逻辑

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,18 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
 import { useDispatch } from 'react-redux'
 import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const onFinish = (values) => {
-    dispatch(fetchLogin(values))
+  const onFinish = async (values) => {
+    await dispatch(fetchLogin(values))
+    navigate('/')
+    message.success('login success')
   }
   return (
     <div className="login">
