5edfd24873c4969bcb2227d81fa68530fa83b15c
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-10 18:00:16 -0500

    P104 Day7-09 Layout-处理token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-import { getToken } from "./token";
+import { getToken, removeToken } from "./token";
+import router from "@/router"
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -26,9 +27,15 @@ request.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
   return response;
-}, function (error) {
+}, (error) => {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
+  console.dir(error)
+  if (error.response.status === 401) {
+    removeToken()
+    router.navigate('/login')
+    window.location.reload()
+  }
   return Promise.reject(error);
 });
 
